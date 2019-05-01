9e7b6e4d21a07d6f8487b7d66587d56a8f883ae3
Julius Zerwick <jrzerwick@gmail.com>
2019-04-30 20:18:05 -0400

    Wrote boiler plate code for greet_server and greet_client

---
+++ greet/greet_client/client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-go-course/greet/greetpb"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+
+}
