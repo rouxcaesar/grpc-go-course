71ce871cad0f16f04d1df42dced5bed66f99d3ff
rouxcaesar <jrzerwick@gmail.com>
2019-05-06 17:10:46 -0400

    Implemented server streaming for calculator

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -1,35 +1,58 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "google.golang.org/grpc"
-  "grpc-go-course/calculator/calculatorpb"
-  "log"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-go-course/calculator/calculatorpb"
+	"log"
+  "io"
 )
 
 func main() {
-  fmt.Println("Calculator Client")
-  cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-  if err != nil {
-    log.Fatalf("Could not connect: %v", err)
-  }
-  defer cc.Close()
+	fmt.Println("Calculator Client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer cc.Close()
 
-  c := calculatorpb.NewCalculatorServiceClient(cc)
+	c := calculatorpb.NewCalculatorServiceClient(cc)
 
-  doUnary(c)
+	// doUnary(c)
+	doServerStreaming(c)
 }
 
 func doUnary(c calculatorpb.CalculatorServiceClient) {
-  fmt.Println("Start to do a Sum Unary RPC...")
-  req := &calculatorpb.SumRequest {
-      FirstNumber: 5,
-      SecondNumber: 40,
-  }
-  res, err := c.Sum(context.Background(), req)
-  if err != nil {
-    log.Fatalf("Error while calling Sum RPC: %v", err)
-  }
-  log.Printf("Response from Sum: %v", res.SumResult)
+	fmt.Println("Start to do a Sum Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  5,
+		SecondNumber: 40,
+	}
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Sum RPC: %v", err)
+	}
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a PrimeDecomposition Server Streaming RPC...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 12390392840,
+	}
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling PrimeNumberDecomposition RPC: %v", err)
+	}
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Something happened: %v", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
 }
--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -1,39 +1,59 @@
 package main
 
 import (
-  "fmt"
-  "context"
-  "log"
-  "net"
-  "google.golang.org/grpc"
-  "grpc-go-course/calculator/calculatorpb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-go-course/calculator/calculatorpb"
+	"log"
+	"net"
 )
 
-type server struct {}
+type server struct{}
 
 func (*server) Sum(ctx context.Context, req *calculatorpb.SumRequest) (*calculatorpb.SumResponse, error) {
-  fmt.Printf("Received Sum RPC: %v\n", req)
-  firstNumber := req.FirstNumber
-  secondNumber := req.SecondNumber
-  sum := firstNumber + secondNumber
-  res := &calculatorpb.SumResponse {
-    SumResult: sum,
-  }
-  return res, nil
+	fmt.Printf("Received Sum RPC: %v\n", req)
+	firstNumber := req.FirstNumber
+	secondNumber := req.SecondNumber
+	sum := firstNumber + secondNumber
+	res := &calculatorpb.SumResponse{
+		SumResult: sum,
+	}
+	return res, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculatorpb.PrimeNumberDecompositionRequest, stream calculatorpb.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Printf("Received PrimeNumberDecomposition RPC: %v\n", req)
+
+	number := req.GetNumber()
+	divisor := int64(2)
+
+	for number > 1 {
+		if number%divisor == 0 {
+			stream.Send(&calculatorpb.PrimeNumberDecompositionResponse{
+				PrimeFactor: divisor,
+			})
+			number = number / divisor
+		} else {
+			divisor += 1
+			fmt.Printf("Divisor has increased to %v\n", divisor)
+		}
+	}
+	return nil
 }
 
 func main() {
-  fmt.Println("Calculator Server")
+	fmt.Println("Calculator Server")
 
-  listener, err := net.Listen("tcp", "0.0.0.0:50051")
-  if err != nil {
-    log.Fatalf("Failed to listen: %v", err)
-  }
+	listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
-  s := grpc.NewServer()
-  calculatorpb.RegisterCalculatorServiceServer(s, &server{}) 
+	s := grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
 
-  if err := s.Serve(listener); err != nil {
-    log.Fatalf("Failed to server: %v", err)
-  }
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	}
 }
